4b6934eadf8bd668ce191301779c78e8a42bd09a
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-21 16:35:50 -0400

    fixed typos

--- models/place.py
+++ models/place.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from models.city import City
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey='cities.id', nullable=False)
-    user_id = Column(String(60), ForeignKey='users.id', nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
     number_rooms = Column(Integer, default=0, nullable=False)
