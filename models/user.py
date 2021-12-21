a535f3bc20dadf9c9f3891d671e363b058f86049
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-12-21 21:09:44 +0000

    Reverting

--- models/user.py
+++ models/user.py
@@ -2,7 +2,6 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -10,10 +9,5 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    places = relationship(
-            "Place", backref='user', cascade="all, delete, delete-orphan")
-    reviews = relationship(
-            "Review", backref='user', cascade="all, delete, delete-orphan")
+    first_name = Column(String(128))
+    last_name = Column(String(128))
