4f743c34170b8958e33271bffdef853a88eca281
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-21 16:28:50 -0400

    fixed imports

--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
