29fcd06ef07876093821b0f3641116a8f1845908
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-12-22 19:49:23 +0000

    Add many to may relationship

--- models/place.py
+++ models/place.py
@@ -4,8 +4,18 @@ import os
 import models
 from models.review import Review
 from models.base_model import BaseModel, Base
-from sqlalchemy import ForeignKey, String, Float, Column, Integer
+from sqlalchemy import ForeignKey, String, Float, Column, Integer, Table
 from sqlalchemy.orm import relationship
+
+
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
