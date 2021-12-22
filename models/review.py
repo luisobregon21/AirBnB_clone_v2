c1ef89b7e5aa61dc932cec73a5814b099c484ba8
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-22 14:51:12 -0400

    updated reviews for task 9

--- models/review.py
+++ models/review.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
