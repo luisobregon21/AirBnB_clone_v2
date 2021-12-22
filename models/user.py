f37675c0b9c9bcea21d8147351d6722f13c7039b
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-22 14:46:23 -0400

    reverted back to task 7

--- models/user.py
+++ models/user.py
@@ -14,6 +14,4 @@ class User(BaseModel, Base):
     last_name = Column(String(128))
 
     places = relationship(
-            "Place", backref='user', cascade="all, delete, delete-orphan")
-    reviews = relationship(
-            "Review", backref='user', cascade="all, delete, delete-orphan")
+        "Place", backref='user', cascade="all, delete, delete-orphan")
