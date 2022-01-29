98839346cb0ecf34115601a16e90e50c1ce98e6f
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-29 05:08:16 +0000

    Modify return

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -24,9 +24,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_txt(text):
     ''' prints value of txt '''
-    if text:
-        text.replace('_', ' ')
-    return "C {}".format(text)
+    return "C {}".format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
