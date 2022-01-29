13491100d2cd118f22a4fd9f4e2f451c8220f6bc
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-29 00:28:57 +0000

    add task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+''' starts flash app '''
+
+# importing Flask task
+from flask import Flask
+from flask import render_template
+
+# instance of the class
+app = Flask(__name__)
+
+# route to tell what URL should trigger the function
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    ''' prints hello hbnb '''
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' prints HBNB'''
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_txt(text):
+    ''' prints C followed by value of txt '''
+    if text:
+        text.replace('_', ' ')
+    return "C {}".format(text)
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    ''' prints python followed by text'''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    ''' prints python followed by text'''
+    return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+        return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
